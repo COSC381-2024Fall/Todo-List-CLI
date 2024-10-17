eaa9b92a21343f66d080802b1cdde9a9fdab2f3f
Siyuan Jiang <144061513+teststudentcosc381@users.noreply.github.com>
2024-10-17 15:22:30 +0000

    Add a function to add a due date to a task
    1. this function will add or update the due date
    2. this function takes the task id and a due date as inputs
    3. this function copies the code from delete_task to check task id
    4. the due date does not have any type constraint
    
    issue #1

--- todo.py
+++ todo.py
@@ -17,6 +17,21 @@ class TodoList:
             for idx, task in enumerate(self.tasks, start=1):
                 print(f'{idx}. {task}')
 
+    def add_task_date(self, task_number, due_date):
+        """Add a due date to a task."""
+        if task_number <= 0 or task_number > len(self.tasks):
+            print("Invalid task number!")
+        else:
+            task = self.tasks[task_number - 1]
+            if type(task) is str:
+                self.tasks[task_number - 1] = (task, due_date)
+            else:
+                task_name = task[0]
+                self.tasks[task_number - 1] = (task_name, due_date)
+            
+            print(f'Task updated: {self.tasks[task_number - 1]}')
+
+
     def delete_task(self, task_number):
         """Deletes a task by its number in the list."""
         if task_number <= 0 or task_number > len(self.tasks):
